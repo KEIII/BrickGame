dd673ee717f374b7dd369e609c0f3c7be9e790b2
Ivan Kasenkov <kacenkov7@gmail.com>
2025-03-28 11:36:04 +0000

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,42 @@
-export default {
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "BrickGame",
+        short_name: "BrickGame",
+        description: "Tile-matching Puzzle Game",
+        theme_color: "#111111",
+        background_color: "#111111",
+        icons: [
+          {
+            src: "src/favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+        ],
+        screenshots: [
+          {
+            src: "screenshots/narrow.png",
+            sizes: "750x1334",
+            form_factor: "narrow",
+          },
+          {
+            src: "screenshots/wide.png",
+            sizes: "3840x2160",
+            form_factor: "wide",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: "127.0.0.1", // Devcontainer workaround https://github.com/vitejs/vite/issues/16522#issuecomment-2084322973
   },
-};
+});
